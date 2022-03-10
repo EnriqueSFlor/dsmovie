f10300d3940e4757a4e103a1d7cd30af9d4cbaaf
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-03-09 22:55:59 -0300

    MovieCard

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,12 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
 
 export default function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+      <MovieCard />
+    </>
+  );
 }
